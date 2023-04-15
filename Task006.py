83b2ea9a8efffd1d6d40ea82d81e2114a7bceba4
VjacheslavRogov <slava.eogov.98@gmail.com>
2023-04-15 16:22:36 +0300

    Completed Task004

--- Task006.py
+++ Task006.py
@@ -7,3 +7,38 @@
 
 # 385916 -> yes
 # 123456 -> no
+
+def sum_digit_in_numbers(number):
+    first_num = number // 100
+    second_num = number % 100 // 10
+    third_num = number % 10
+    sum = first_num + second_num + third_num
+    return sum
+
+num = int(input("Введите шестизначное число: "))
+if num < 1000000 and num > 99999:
+    first_half_num = num // 1000
+    second_half_num = num % 1000
+
+    first_sum = sum_digit_in_numbers(first_half_num)
+    second_sum = sum_digit_in_numbers(second_half_num)
+
+    if first_sum == second_sum:
+        print('YES')
+    else:
+        print('NO')
+else:
+    print('Введены неверные данные')
+
+
+
+
+
+
+
+
+
+    # first_num = num // 100
+    # second_num = num % 100 // 10
+    # third_num = num % 10
+    # sum = first_num + second_num + third_num
